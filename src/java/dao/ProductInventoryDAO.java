cceff04a32f996ad2215b6e25a22f8c4c2a3213c
hoangdietkid <hoangdietkid@gmail.com>
2025-03-24 03:31:42 +0700

    payment

--- src/java/dao/ProductInventoryDAO.java
+++ src/java/dao/ProductInventoryDAO.java
@@ -4,16 +4,75 @@
  */
 package dao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
 import model.ProductInventory;
 
 /**
  *
  * @author anhqu
  */
-public class ProductInventoryDAO extends GenericDAO<ProductInventory>{
-    
-     public ProductInventoryDAO() {
+public class ProductInventoryDAO extends GenericDAO<ProductInventory> {
+
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("MedicineAppPU");
+
+    public ProductInventoryDAO() {
         super(ProductInventory.class);
     }
-    
+
+    public int findQuantityByProductId(int productId) {
+        EntityManager em = getEntityManager();
+        try {
+            String query = "SELECT p.quantity FROM ProductInventory p WHERE p.productId.productId = :productId";
+            TypedQuery<Integer> typedQuery = em.createQuery(query, Integer.class);
+            typedQuery.setParameter("productId", productId);
+            Integer quantity = typedQuery.getSingleResult();
+            return quantity != null ? quantity : 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return 0;
+        } finally {
+            em.close();
+        }
+    }
+
+    public boolean subtractQuantity(int productId, int quantityToSubtract) {
+        EntityManager em = emf.createEntityManager();
+        EntityTransaction transaction = em.getTransaction();
+        try {
+            transaction.begin();
+
+            ProductInventory productInventory = em.find(ProductInventory.class, productId);
+
+            if (productInventory != null) {
+                int currentQuantity = productInventory.getQuantity();
+
+                if (currentQuantity >= quantityToSubtract) {
+                    int newQuantity = currentQuantity - quantityToSubtract;
+                    productInventory.setQuantity(newQuantity);
+
+                    em.merge(productInventory);
+                    transaction.commit();
+                    return true;
+                } else {
+                    System.out.println("Số lượng không đủ để trừ.");
+                    return false;
+                }
+            } else {
+                System.out.println("Sản phẩm không tồn tại.");
+                return false;
+            }
+        } catch (Exception e) {
+            if (transaction.isActive()) {
+                transaction.rollback();
+            }
+            e.printStackTrace();
+            return false;
+        } finally {
+            em.close();
+        }
+    }
 }
